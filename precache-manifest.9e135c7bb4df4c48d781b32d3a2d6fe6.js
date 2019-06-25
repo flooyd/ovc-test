946a4571d0c2f201b9d7ebe09d026b02a0890ad0
Floyd Jones <floydtjones@gmail.com>
2019-06-25 14:38:33 -0500

    Updates

---
+++ precache-manifest.9e135c7bb4df4c48d781b32d3a2d6fe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e59c3da50007c9719c4dbfa85b93c9",
+    "url": "/ovc-test/index.html"
+  },
+  {
+    "revision": "a44708e8e70a9f645194",
+    "url": "/ovc-test/static/css/main.3c02c300.chunk.css"
+  },
+  {
+    "revision": "083354fa067f4d80d54c",
+    "url": "/ovc-test/static/js/2.a6ce0543.chunk.js"
+  },
+  {
+    "revision": "a44708e8e70a9f645194",
+    "url": "/ovc-test/static/js/main.9990fe2b.chunk.js"
+  },
+  {
+    "revision": "6764aa10217747fafec6",
+    "url": "/ovc-test/static/js/runtime~main.616aa0b7.js"
+  }
+]);
